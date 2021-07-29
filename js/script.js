30c6da348c10b3991cdc22fc418e0d0a0b0a6121
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-07-29 15:12:23 +0200

    Aggiunta prompt km ed età dell'utente

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// 1. Chiedere all'utente il numero di chilometri che vuole percorrere
+var chilometri = prompt("Quanti chilomentri desideri percorrere?");
+
+// 2. Chiedere all'utente l'età del passeggero
+var età = prompt("Quanti anni hai?")
+
+// 3. Calcolo prezzo biglietto in base ai chilomentri da percorrere

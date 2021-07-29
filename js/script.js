3c4a507f71824f0a54e1a4dedbbf1b1508c1870b
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-07-29 15:37:18 +0200

    Aggiunta calcolo biglietto in base ai km

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // 1. Chiedere all'utente il numero di chilometri che vuole percorrere
-var chilometri = prompt("Quanti chilomentri desideri percorrere?");
+var chilometri = parseInt(prompt("Quanti chilomentri desideri percorrere?"));
 
 // 2. Chiedere all'utente l'età del passeggero
-var età = prompt("Quanti anni hai?")
+var età = parseInt(prompt("Quanti anni hai?"));
 
 // 3. Calcolo prezzo biglietto in base ai chilomentri da percorrere
+var prezzo = (chilometri * 0.21);
+document.getElementById('biglietto').innerHTML += prezzo;

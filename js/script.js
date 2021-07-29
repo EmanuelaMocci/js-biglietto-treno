98127b62dc4fb113d9735238e5d89f10f1c88867
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-07-29 16:14:09 +0200

    Aggiunta condizioni prezzo

--- js/script.js
+++ js/script.js
@@ -2,14 +2,26 @@
 var chilometri = parseInt(prompt("Quanti chilomentri desideri percorrere?"));
 
 // 2. Chiedere all'utente l'età del passeggero
-var età = parseInt(prompt("Quanti anni hai?"));
+var eta = parseInt(prompt("Quanti anni hai?"));
 
 // 3. Calcolo prezzo biglietto in base ai chilomentri da percorrere
 var prezzo = (chilometri * 0.21);
-document.getElementById('biglietto').innerHTML += prezzo;
+// document.getElementById('biglietto').innerHTML += prezzo;
 
 // 4. Applicazione sconto del 20% per i minorenni
 var sconto = (prezzo / 100) * 20; 
-var prezzoFinale = (prezzo - sconto);
-document.getElementById('sconto-minorenni').innerHTML += prezzoFinale;
+var prezzoFinaleMinorenne = (prezzo - sconto);
+// document.getElementById('sconto-minorenni').innerHTML += prezzoFinale;
 
+// 5. Applicazione sconto del 40% se over 65
+var sconto = (prezzo / 100) * 40; 
+var prezzoFinale = (prezzo - sconto);
+// document.getElementById('sconto-adulto').innerHTML += prezzoFinale;
+
+if( eta < 18){
+    document.getElementById('biglietto').innerHTML += prezzoFinaleMinorenne;
+} else if ( eta > 65){
+    document.getElementById('biglietto').innerHTML += prezzoFinale;
+} else {
+    document.getElementById('biglietto').innerHTML += prezzo;
+}

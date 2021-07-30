922be6c525980c459da725c8184f7ba91e06201f
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-07-30 10:38:46 +0200

    Fix nome variabili

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@ var eta = parseInt(prompt("Quanti anni hai?"));
 // 3. Calcolo prezzo biglietto in base ai chilomentri da percorrere
 var prezzo = (chilometri * 0.21);
 
-// 4. Applicazione sconto del 20% per i minorenni
-var sconto = (prezzo / 100) * 20; 
-var prezzoFinaleMinorenne = (prezzo - sconto).toFixed(2);
+// 4. Applicazione sconto del 20% per i minorenni 
+// (oppure: var sconto = 100 * 0.20)
+var sconto20 = (prezzo / 100) * 20; 
+// (oppure: (prezzo -= sconto))
+var prezzoFinaleMinorenne = (prezzo - sconto20).toFixed(2);
 
 // 5. Applicazione sconto del 40% se over 65
-var sconto = (prezzo / 100) * 40; 
-var prezzoFinale = (prezzo - sconto).toFixed(2);
+var sconto40 = (prezzo / 100) * 40; 
+var prezzoFinale = (prezzo - sconto40).toFixed(2);
 
 // 6. Aggiunta condizione 
 if( eta < 18){
